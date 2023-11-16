1263d0a1d56c1103d6588c75b904499cc320020d
Akash Kumar <meakash7902@gmail.com>
2023-11-16 12:27:16 +0530

    feat: add article schema
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

---
+++ schemas/article.js
@@ -0,0 +1,38 @@
+export default {
+    name: 'article',
+    title: 'Articles',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+        },
+        {
+            name: 'content',
+            title: 'Content',
+            type: 'text'
+        },
+        {
+            name: 'author',
+            title: 'Author',
+            type: 'reference',
+            to: [{ type: 'user' }]
+        },
+        {
+            name: 'createdDate',
+            title: 'CreatedDate',
+            type: 'date'
+        },
+        {
+            name: 'editedDate',
+            title: 'EditedDate',
+            type: 'date'
+        }
+    ]
+}
--- schemas/index.js
+++ schemas/index.js
@@ -3,5 +3,6 @@ import tree from './tree.js'
 import reward from './reward.js'
 import rewardgiven from './rewardgiven.js'
 import watereddata from './watereddata.js'
+import article from './article.js'
 
-export const schemaTypes = [user, tree, reward, rewardgiven, watereddata]
+export const schemaTypes = [user, tree, reward, rewardgiven, watereddata, article]
